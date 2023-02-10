08054382cfb4b5583a9176b5880a19bfd1cb5ca8
Mozartus <lukas.kleybolte@hs-augsburg.de>
2023-02-10 19:20:48 +0100

    :rocket: init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from 'react';
+import "./styles/App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import EmotionEmojis from "./SVGSwitcher";
+import {Pace, StyledText, WindupChildren} from "windups";
+
+const App = () => {
+    const [index, setIndex] = useState(0);
+    const emotions = ["happy", "relaxed", "sad", "angry"];
+    const [accuracy, setAccuracy] = useState(0);
+    useEffect(() => {
+        setAccuracy(Math.random() * 100);
+    }, [index]);
+
+
+    return (
+        <div style={{height: '100vh'}}>
+            <div style={{
+                height: "100vh",
+                display: 'flex',
+                flexDirection: "column",
+                alignItems: 'center',
+                justifyContent: 'center',
+                background: '#FFCD4C 60%'
+            }} onClick={() => setIndex((index + 1) % 4)}>
+                <div className={"EmotionName"}>{accuracy.toFixed(2)}%</div>
+                <EmotionEmojis index={index}/>
+                <WindupChildren>
+                    <div
+                        key={index}
+                        className={"EmotionName"}
+                    >
+                        <Pace ms={100}>{emotions[index]}</Pace>
+                    </div>
+                </WindupChildren>
+            </div>
+        </div>
+    );
+};
 
 export default App;
+
